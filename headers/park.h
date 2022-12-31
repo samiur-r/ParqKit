73e43648c488da4aeb67a4e235657f92bea5e437
samiur-r <samiurrahman.dev@gmail.com>
2022-12-31 17:42:03 +0600

    chore: update header file for parking module

--- headers/park.h
+++ headers/park.h
@@ -2,5 +2,7 @@
 #define HEADERS_PARK_H_
 
 int park();
+int add_lot(struct parking *p, int available_lots, int occupied);
+int free_lot(struct parking *p, int lot_num);
 
 #endif /* HEADERS_PARK_H_ */
